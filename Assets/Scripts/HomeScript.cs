34be2a01827c71875a06498268b6fec3230b2799
Jake <javakatdesign@gmail.com>
2014-01-20 13:16:52 -0600

    Initial commit

---
+++ Assets/Scripts/HomeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeScript : MonoBehaviour {
+
+    void OnGUI()
+    {
+        if (GUI.Button(
+            new Rect(
+                0,
+                0,
+                Screen.width / 3,
+                Screen.height
+            ), "Adventure!")
+        )
+        {
+            int random = Random.Range(1, Application.levelCount);
+            Application.LoadLevel(random);
+        }
+    }
+}
